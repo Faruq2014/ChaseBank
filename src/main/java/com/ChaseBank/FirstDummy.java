ee0dca8049e71c547f45cf36ae18c8cb3aa9f28f
Faruq2014 <mjmolla@gmail.com>
2021-03-25 21:51:57 -0400

    git ignore file

---
+++ src/main/java/com/ChaseBank/FirstDummy.java
@@ -0,0 +1,10 @@
+package com.ChaseBank;
+
+public class FirstDummy {
+
+	public static void main(String[] args) {
+		System.out.println("Assalamualaikum");
+
+	}
+
+}
---
+++ src/test/java/com/ChaseBank/FirstTest.java
@@ -0,0 +1,10 @@
+package com.ChaseBank;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+@Test
+public void sampaleTest() {
+	System.out.println("Assalamualikum");
+}
+}
