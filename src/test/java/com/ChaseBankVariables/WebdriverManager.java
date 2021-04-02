341dfdf84521d7f4a957b5f55b585bcf085d043d
Younesse <Younesse@10.0.0.137>
2021-04-02 00:09:22 -0400

    Younesse commit localVariables/

---
+++ src/test/java/com/ChaseBankVariables/WebdriverManager.java
@@ -0,0 +1,10 @@
+package com.ChaseBankVariables;
+
+public class WebdriverManager {
+
+	public static Object chromedriver() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/test/java/com/ChaseBankVariables/localVariable.java
@@ -0,0 +1,27 @@
+package com.ChaseBankVariables;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+@Test
+public class localVariable {
+	
+	
+	public void chrometest () {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://locator.chase.com/");
+		System.out.println(driver.getTitle());
+		
+	}
+
+	 
+		
+	}
+
+
+
+
