5da8c9de8deeb17f31f6db9cefe384fb07a7f062
emon <emon@localhost>
2021-04-04 18:38:05 -0400

    Test

--- src/main/java/com/ChaseBankEmon/FirstTest.java
+++ src/main/java/com/ChaseBankEmon/FirstTest.java
@@ -4,7 +4,7 @@ public class FirstTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("This is Emon");
+System.out.println("Emon's Chase Project");
 	}
 
 }
