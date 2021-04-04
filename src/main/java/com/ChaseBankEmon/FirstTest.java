b247b510c4474c731668dc0f5fc863e3307e3952
emon <emon@localhost>
2021-04-04 00:40:43 -0400

    Emon's 1st chase commit

---
+++ src/main/java/com/ChaseBankEmon/FirstTest.java
@@ -0,0 +1,10 @@
+package com.ChaseBankEmon;
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is Emon");
+	}
+
+}
