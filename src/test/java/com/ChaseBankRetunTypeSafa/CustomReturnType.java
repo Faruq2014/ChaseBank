8a173ae5c36b55e7ec08aac32f55b599641df778
Safa Bhuiyan <icesafa@gmail.com>
2021-04-12 01:06:48 -0400

    3rd time committee from safa

--- src/test/java/com/ChaseBankRetunTypeSafa/CustomReturnType.java
+++ src/test/java/com/ChaseBankRetunTypeSafa/CustomReturnType.java
@@ -3,8 +3,24 @@ package com.ChaseBankRetunTypeSafa;
 public class CustomReturnType {
 
 	public static void main(String[] args) {
-System.out.println("test");
+		CustomReturnType custom = new CustomReturnType();
+		String checkString1 = custom.checkStringMethod();
+		String checkString2 = checkString1.toUpperCase();
+		System.out.println(checkString2);
+		System.out.println(checkString1.charAt(1));
 
+		custom.checkVoidMethod();
+
+	}
+
+	public void checkVoidMethod() {
+		System.out.println("This is a method with void return type");
+	}
+
+	public String checkStringMethod() {
+		String checkString = "This is a String retrun type method";
+		System.out.println(checkString);
+		return checkString;
 	}
 
 }
